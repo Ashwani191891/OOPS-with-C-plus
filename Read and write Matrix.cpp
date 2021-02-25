7f36aa7e41a738d5c51746f0b91557d5cbc9ddd6
Ashwani 191891 <60172012+Ashwani191891@users.noreply.github.com>
2021-02-25 21:09:31 +0530

    Add files via upload

---
+++ Read and write Matrix.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+
+using namespace std;
+
+int row_m, col_n, matrix[sizeof(row_m)][sizeof(col_n)];
+
+
+// read_matrix function -- to get input of matrix order and matrix elements from keyboard
+
+int read_matrix(int matrix[sizeof(row_m)][sizeof(col_n)])
+{
+    int i, j;
+
+    for(i=1; i<=row_m; i++)
+    {
+        for(j=1; j<=col_n; j++)
+        {
+            cout<<"\n A" <<i <<j <<"th element : ";
+            cin>>matrix[i][j];
+        }
+    }
+}
+
+
+
+// display_matrix function -- to display or print entered matrix as output
+
+
+int display_matrix(int matrix[sizeof(row_m)][sizeof(col_n)])
+{
+    int i, j;
+
+    for(i=1; i<=row_m; i++)
+    {
+        for(j=1; j<=col_n; j++)
+        {
+           cout<<"\t" <<matrix[i][j];
+        }
+        
+        cout<<endl;
+    
+	}
+
+}
+    
+    
+
+// call the read_matrix and display_matrix functions to perform their tasks
+
+
+int main()
+{
+	cout<<" Enter the number of rows : ";
+    cin>>row_m;
+
+    cout<<" Enter the number of columns : ";
+    cin>>col_n;
+    
+    read_matrix(matrix);
+    
+    cout<<"\n  You have entered following matrix"<<endl;
+    
+    display_matrix(matrix);
+
+}
