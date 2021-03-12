b35fc316a082c42c8656fef3def7d68a4f2a2fcd
Ashwani 191891 <60172012+Ashwani191891@users.noreply.github.com>
2021-03-12 18:54:08 +0530

    Update Read and write Matrix.cpp

--- Read and write Matrix.cpp
+++ Read and write Matrix.cpp
@@ -1,9 +1,7 @@
 #include<iostream>
-
 using namespace std;
 
 int row_m, col_n, matrix[50][50];
-
 
 // read_matrix function -- to get input of matrix order and matrix elements from keyboard
 
@@ -21,10 +19,7 @@ int read_matrix(int matrix[50][50])
     }
 }
 
-
-
 // display_matrix function -- to display or print entered matrix as output
-
 
 int display_matrix(int matrix[50][50])
 {
@@ -36,23 +31,16 @@ int display_matrix(int matrix[50][50])
         {
            cout<<"\t" <<matrix[i][j];
         }
-        
         cout<<endl;
-    
 	}
-
 }
     
-    
-
 // call the read_matrix and display_matrix functions to perform their tasks
-
 
 int main()
 {
-	cout<<" Enter the number of rows : ";
+    cout<<" Enter the number of rows : ";
     cin>>row_m;
-
     cout<<" Enter the number of columns : ";
     cin>>col_n;
     
@@ -61,5 +49,4 @@ int main()
     cout<<"\n  You have entered following matrix"<<endl;
     
     display_matrix(matrix);
-
 }
